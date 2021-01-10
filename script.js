ee0bff2793c0eef0d20f2893d960e22feb6fe030
Michael Warmbier <MichaelCWarmbier@gmail.com>
2021-01-10 12:40:28 -0500

    Test

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ function getCookie(cname) {
   let cookieData;
   cookieData = document.cookie.split(';').map(cookie => cookie.split('='));
   for (i = 0; i < cookieData.length; i++)
-    if (cookieData[i][0] == cname)
+    if (cookieData[i][0] == " " + cname)
       return cookieData[i][1];
   return "error";
 
